f5cb0712bef1884cf023ac38dd212a024fd90f7a
qianlongzt <1401300561@qq.com>
2024-09-16 01:48:25 +0800

    web: simplify listener

--- web/listener.go
+++ web/listener.go
@@ -2,12 +2,13 @@ package web
 
 import (
 	"crypto/tls"
+	"net"
+	"net/http"
+
 	"github.com/coreos/go-systemd/v22/activation"
 	"github.com/go-chi/chi/v5"
 	"github.com/librespeed/speedtest/config"
 	log "github.com/sirupsen/logrus"
-	"net"
-	"net/http"
 )
 
 func startListener(conf *config.Config, r *chi.Mux) error {
@@ -23,25 +24,24 @@ func startListener(conf *config.Config, r *chi.Mux) error {
 	case 0:
 		addr := net.JoinHostPort(conf.BindAddress, conf.Port)
 		log.Infof("Starting backend server on %s", addr)
-
+		srv := &http.Server{
+			Addr:    addr,
+			Handler: r,
+		}
 		// TLS and HTTP/2.
 		if conf.EnableTLS {
 			log.Info("Use TLS connection.")
+
 			if !(conf.EnableHTTP2) {
-				srv := &http.Server{
-					Addr:         addr,
-					Handler:      r,
-					TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
-				}
-				s = srv.ListenAndServeTLS(conf.TLSCertFile, conf.TLSKeyFile)
-			} else {
-				s = http.ListenAndServeTLS(addr, conf.TLSCertFile, conf.TLSKeyFile, r)
+				// If TLSNextProto is not nil, HTTP/2 support is not enabled automatically.
+				srv.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler))
 			}
+			s = srv.ListenAndServeTLS(conf.TLSCertFile, conf.TLSKeyFile)
 		} else {
 			if conf.EnableHTTP2 {
 				log.Errorf("TLS is mandatory for HTTP/2. Ignore settings that enable HTTP/2.")
 			}
-			s = http.ListenAndServe(addr, r)
+			s = srv.ListenAndServe()
 		}
 	case 1:
 		log.Info("Starting backend server on inherited file descriptor via systemd socket activation")
